3fb73e7b5ebd020cae9b75818c3401e3f08867b1
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2017-12-03 09:05:19 +0100

    Configure redux and redux-saga middleware

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,13 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 
 import { Home, Category, PostDetails, CreateEdit } from './screens';
+import reducers from './reducers';
+import sagas from './sagas';
 
 const Wrapper = styled.div`
   margin-left: auto;
@@ -11,20 +16,45 @@ const Wrapper = styled.div`
   max-width: 1240px;
 `;
 
+/*
+|-----------------------------------------------------------
+| Redux
+|-----------------------------------------------------------
+*/
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
+
+const store = createStore(
+  reducers,
+  process.env.NODE_ENV === 'development'
+    ? require('redux-devtools-extension').composeWithDevTools(
+        applyMiddleware(...middleware),
+      )
+    : applyMiddleware(...middleware),
+);
+
+sagaMiddleware.run(sagas);
+
+
 type Props = {};
 type State = {};
 
 class App extends React.Component<Props, State> {
   render() {
     return (
-      <Router>
-        <Wrapper>
-          <Route exact path="/" component={Home} />
-          <Route path="/categories/:category" component={Category} />
-          <Route path="/categories/:category/posts" component={CreateEdit} />
-          <Route path="/categories/:category/posts/:postId" component={PostDetails} />
-        </Wrapper>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Wrapper>
+            <Route exact path="/" component={Home} />
+            <Route path="/categories/:category" component={Category} />
+            <Route path="/categories/:category/posts" component={CreateEdit} />
+            <Route
+              path="/categories/:category/posts/:postId"
+              component={PostDetails}
+            />
+          </Wrapper>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ frontend/src/sagas/index.js
@@ -0,0 +1,8 @@
+/* @flow */
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+
+function* rootSaga() {
+  // yield takeLatest('USER_FETCH_REQUESTED', fetchUser);
+}
+
+export default rootSaga;
